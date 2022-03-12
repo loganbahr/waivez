1ea7592e82685a84231652efe36e82408b9384d9
Devin Arena <devin0309@yahoo.com>
2022-03-12 00:21:06 -0500

    get endpoint for user table

--- server/index.js
+++ server/index.js
@@ -7,6 +7,18 @@
 const PORT = 5000; // still kinda undecided, consult with Logan
 
 const express = require('express');
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const pool = new Pool(
+    {
+        user: process.env.PGUSER,
+        host: process.env.PGHOST,
+        database: process.env.PGDB,
+        password: process.env.PGPASS,
+        port: process.env.PGPORT,
+    }
+);
 
 // Gonna have to fix cors eventually.
 const app = express();
@@ -14,8 +26,25 @@ const app = express();
 /**
  * Test request to ping the server.
  */
-app.get('/hello', (req, res) => {
-    res.send('hi!');
+app.get('/user/:id', (req, resp) => {
+    const id = req.params.id;
+
+    pool.query('SELECT * FROM users WHERE id=$1', [id], (err, res) => {
+        if (err) {
+            resp.send(err.message);
+        } else {
+            resp.send({
+                id: id,
+                first_name: res.rows[0].first_name,
+                last_name: res.rows[0].last_name,
+                date_of_birth: res.rows[0].date_of_birth,
+                address: res.rows[0].address,
+                email: res.rows[0].email,
+                phone_number: res.rows[0].phone_number,
+            });
+        }
+        pool.end();
+    });
 });
 
 app.listen(PORT);
