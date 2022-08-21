c43087110b758306e8ff0f6e3b3e46377555e2d3
unknown <loganbahr94@gmail.com>
2022-08-21 11:00:35 -0400

    testing nextauth problems
    Took 7 minutes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -66,7 +66,5 @@ export default NextAuth({
     pages: {
         signIn: '/auth/signin',
     },
-    secret: process.env.NEXTAUTH_SECRET,
-
-
+    secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
 })
