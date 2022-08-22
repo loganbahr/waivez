59329f51e10b1efd88519c39bf0d9ea8c5016d86
unknown <loganbahr94@gmail.com>
2022-08-22 15:32:59 -0400

    nextauth test again
    Took 2 minutes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,7 +10,7 @@ import {connectToDatabase} from "../../../lib/db";
 import {comparePassword} from "../../../lib/auth";
 
 export default NextAuth({
-    secret: process.env.NEXT_PUBLIC_SECRET,
+    secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
     providers: [
         CredentialsProvider({
             name: "credentials",
