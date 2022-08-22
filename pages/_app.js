db850ce928cfd025847cfdabb8d8ee756abb4a42
unknown <loganbahr94@gmail.com>
2022-08-22 09:30:40 -0400

    nextauth testing, SessionProvier
    Took 13 minutes

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ const MyApp = ({Component, pageProps: {session, ...pageProps}}) => {
 
     return (
         <>
-            <SessionProvider session={session}>
+            <SessionProvider session={session} basePath={process.env.NEXTAUTH_URL}>
                 <ThemeProvider theme={theme}>
                     <CssBaseline/>
                     <Layout>
