2dc8199f0806faa868b9b6001e345a1f8cdc1659
Logan Bahr <loganbahr94@gmail.com>
2022-09-14 11:59:46 -0400

    testing router for dashboard bug

--- components/Pages/Home/TailwindFeaturesSection.js
+++ components/Pages/Home/TailwindFeaturesSection.js
@@ -1,78 +1,89 @@
-/* This example requires Tailwind CSS v2.0+ */
-import {
-    CloudUploadIcon,
-    CogIcon,
-    LockClosedIcon,
-    RefreshIcon,
-    ServerIcon,
-    ShieldCheckIcon,
-} from '@heroicons/react/outline'
-import {CheckIcon} from "@heroicons/react/solid";
+/**
+ * @file components\Pages\Home\TailwindFeaturesSection.js
+ * @author: Logan Bahr
+ * @description: Some various features of Waivez.
+ * @since 7/26/22
+ */
+import { CheckIcon } from "@heroicons/react/solid";
 
 const features = [
-    {
-        name: 'Return Probability Index',
-        description: 'An analytics tool that predicts the likelihood of a customer to return, and when they are most likely to return.',
-        icon: CheckIcon,
-    },
-    {
-        name: 'Customer Heat Maps',
-        description: 'An interactive map of the United States that depicts where your customers are coming from over a given period of time.',
-        icon: CheckIcon,
-    },
-    {
-        name: 'Advanced Analytics',
-        description: 'Take advantage of Machine Learning powered algorithms to identify trends and patterns in your data.',
-        icon: CheckIcon,
-    },
-    {
-        name: 'Interactive Partner Dashboard',
-        description: "Access to your own Partner Dashboard — a place where all of your customer data is stored, managed, and analyzed.",
-        icon: CheckIcon,
-    },
-    {
-        name: 'Customer Support',
-        description: 'Our customer support team is available 24/7 to help you with any questions you may have.',
-        icon: CheckIcon,
-    },
-    {
-        name: 'Third Party Integrations',
-        description: "Integrate with third party services to automate your business processes and increase your revenue.",
-        icon: CheckIcon,
-    },
-]
+  {
+    name: "Return Probability Index",
+    description:
+      "An analytics tool that predicts the likelihood of a customer to return, and when they are most likely to return.",
+    icon: CheckIcon,
+  },
+  {
+    name: "Customer Heat Maps",
+    description:
+      "An interactive map of the United States that depicts where your customers are coming from over a given period of time.",
+    icon: CheckIcon,
+  },
+  {
+    name: "Advanced Analytics",
+    description:
+      "Take advantage of Machine Learning powered algorithms to identify trends and patterns in your data.",
+    icon: CheckIcon,
+  },
+  {
+    name: "Interactive Partner Dashboard",
+    description:
+      "Access to your own Partner Dashboard — a place where all of your customer data is stored, managed, and analyzed.",
+    icon: CheckIcon,
+  },
+  {
+    name: "Customer Support",
+    description:
+      "Our customer support team is available 24/7 to help you with any questions you may have.",
+    icon: CheckIcon,
+  },
+  {
+    name: "Third Party Integrations",
+    description:
+      "Integrate with third party services to automate your business processes and increase your revenue.",
+    icon: CheckIcon,
+  },
+];
 
 export default function TailwindFeaturesSection() {
-    return (
-        <div className="relative bg-secondary py-16 sm:py-24 lg:py-32">
-            <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
-                <p className="mt-2 text-3xl font-extrabold tracking-tight text-primary sm:text-4xl">
-                    The most advanced waiver technology in the industry.
-                </p>
-                <p className="mx-auto mt-5 max-w-prose text-xl text-gray-800">
-                    Get to know your customers and understand your business&apos;s demographic.
-                    Learn where your customers are coming from — and when they&apos;ll be back.
-                </p>
-                <div className="mt-12">
-                    <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-                        {features.map((feature) => (
-                            <div key={feature.name} className="pt-6">
-                                <div className="flow-root rounded-lg bg-gray-50 px-6 pb-8">
-                                    <div className="-mt-6">
-                                        <div>
-                      <span className="inline-flex items-center justify-center rounded-md bg-primary p-3 shadow-lg">
-                        <feature.icon className="h-6 w-6 text-secondary" aria-hidden="true"/>
+  return (
+    <div className="relative py-16 bg-secondary sm:py-24 lg:py-32">
+      <div className="max-w-md px-4 mx-auto text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+        <p className="mt-2 text-3xl font-extrabold tracking-tight text-primary sm:text-4xl">
+          The most advanced waiver technology in the industry.
+        </p>
+        <p className="mx-auto mt-5 text-xl text-gray-800 max-w-prose">
+          Get to know your customers and understand your business&apos;s
+          demographic. Learn where your customers are coming from — and when
+          they&apos;ll be back.
+        </p>
+        <div className="mt-12">
+          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            {features.map((feature) => (
+              <div key={feature.name} className="pt-6">
+                <div className="flow-root px-6 pb-8 rounded-lg bg-gray-50">
+                  <div className="-mt-6">
+                    <div>
+                      <span className="inline-flex items-center justify-center p-3 rounded-md shadow-lg bg-primary">
+                        <feature.icon
+                          className="w-6 h-6 text-secondary"
+                          aria-hidden="true"
+                        />
                       </span>
-                                        </div>
-                                        <h3 className="mt-8 text-lg font-medium tracking-tight text-gray-900">{feature.name}</h3>
-                                        <p className="mt-5 text-base text-gray-500">{feature.description}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
                     </div>
+                    <h3 className="mt-8 text-lg font-medium tracking-tight text-gray-900">
+                      {feature.name}
+                    </h3>
+                    <p className="mt-5 text-base text-gray-500">
+                      {feature.description}
+                    </p>
+                  </div>
                 </div>
-            </div>
+              </div>
+            ))}
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -2,24 +2,30 @@ import Footer from "./Footer/Footer";
 import { useSession } from "next-auth/react";
 import TailwindHeader from "./Header/TailwindHeader";
 import TailwindFooter from "./Footer/TailwindFooter";
+import { useRouter } from "next/router";
 
 const Layout = (props) => {
   const { data: session, status } = useSession();
 
   //console.log(props.children.type.displayName);
 
+  const router = useRouter();
+
   return (
     <div>
       {/*if there is no session and they are not on the dashboard*/}
-      {props.children.type.displayName !== "Dashboard" ? (
+      {/* {props.children.type.displayName !== "Dashboard" ? (
         <TailwindHeader />
-      ) : undefined}
+      ) : undefined} */}
+
+      {!router.pathname.includes("dashboard") ? <TailwindHeader /> : undefined}
 
       <div className={"min-h-screen"}>{props.children}</div>
 
-      {props.children.type.displayName !== "Dashboard" ? (
+      {/* {props.children.type.displayName !== "Dashboard" ? (
         <TailwindFooter />
-      ) : undefined}
+      ) : undefined} */}
+      {!router.pathname.includes("dashboard") ? <TailwindFooter /> : undefined}
     </div>
   );
 };
